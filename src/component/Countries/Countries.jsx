f7425379041de54b49a96295d245b92f0aa80e8f
Puspol Mondal <acc.puspol88@gmail.com>
2025-09-22 11:32:33 +0600

    visited ccountry show

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -7,11 +7,21 @@ const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
   const handelVisitedCountry = (country) => {
-    console.log(country);
+    const newvisited = [...visitedCountry, country];
+    setVisitedCountry(newvisited);
   };
+
   return (
     <div className=" w-11/12 mx-auto">
       <h1>Hello world: {countries.length}</h1>
+      <h3 className=" text-3xl my-2">
+        Visited country count: {visitedCountry.length}
+      </h3>
+      <ol>
+        {visitedCountry.map((country) => (
+          <li key={country.cca3.cca3}>{country.name.common}</li>
+        ))}
+      </ol>
       <div className=" countries ">
         {countries.map((res) => (
           <Country
